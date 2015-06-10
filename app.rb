498a2086fb2712df4cbae6a370fc418cc5963c9f
Nicholas Emanuel <njemanuel01@gmail.com>
2015-06-10 13:08:18 -0500

    Deleted validator. Added a method to fetch a character from the marvel API.

--- app.rb
+++ app.rb
@@ -5,10 +5,5 @@ character_name = gets.chomp
 
 session = Marvel.new()
 
-while session.valid_character == false
-  puts "Invalid character name.  Please enter a valid character name."
-  puts "Here's a list."
-  puts session.characters
-  character_name = gets.chomp
-end
+puts session.fetch_character(character_name)
   
--- marvel.rb
+++ marvel.rb
@@ -5,11 +5,10 @@ class Marvel
   def initialize()
     @client = Marvelite::API::Client.new( :public_key => '2b4b1e601cc382721ce8b4fceb18494c', 
     :private_key => 'b83361c174682e6fc1488982737c9c564002657a')
-    @characters = @client.characters
   end
   
-  def valid_character(character_name)
-    @characters.include?(character_name)
+  def fetch_character(character_name)
+    @client.character(character_name)
   end
     
 end
