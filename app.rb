74777a393aae048dc8b62b4ca9a826c1f1134ff3
Nicholas Emanuel <njemanuel01@gmail.com>
2015-06-10 14:10:40 -0500

    Added name validator.

--- app.rb
+++ app.rb
@@ -1,10 +1,15 @@
 require_relative "marvel.rb"
 
+session = Marvel.new()
+
 puts "Enter the name of a Marvel Universe Character."
 character_name = gets.chomp
 
-session = Marvel.new()
+while ((session.check_character_name(character_name)) == 404)
+  puts "Invalid entry.  Please try again."
+  character_name = gets.chomp
+end
 
-puts session.fetch_character_name(character_name)
+puts "Here's a description for #{character_name}:"
 puts session.fetch_character_description(character_name)
   
--- marvel.rb
+++ marvel.rb
@@ -1,20 +1,19 @@
 require "marvelite"
 
 class Marvel
-  attr_reader :characters
   def initialize()
     @client = Marvelite::API::Client.new( :public_key => '2b4b1e601cc382721ce8b4fceb18494c', 
     :private_key => 'b83361c174682e6fc1488982737c9c564002657a')
   end
   
-  def fetch_character_name(character_name)
+  def check_character_name(character_name)
     hero = @client.character(character_name)
-    hero.data[:results][0][:name]
+    hero[:code]
   end
   
   def fetch_character_description(character_name)
-    hero = @client.character(character_name)
-    hero.data[:results][0][:description]
+    @hero = @client.character(character_name)
+    @hero.data[:results][0][:description]
   end
     
 end
