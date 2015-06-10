ece3df5f223a759219654f691f0c75137b80db3c
Nicholas Emanuel <njemanuel01@gmail.com>
2015-06-10 15:34:19 -0500

    Stopped screwing around trying to make an array of characters and finished.

--- app.rb
+++ app.rb
@@ -1,15 +1,21 @@
 require_relative "marvel.rb"
 
-session = Marvel.new()
-
 puts "Enter the name of a Marvel Universe Character."
 character_name = gets.chomp
+
+session = Marvel.new(character_name)
 
 while ((session.check_character_name(character_name)) == 404)
   puts "Invalid entry.  Please try again."
   character_name = gets.chomp
+  session = Marvel.new(character_name)
 end
 
 puts "Here's a description for #{character_name}:"
 puts session.fetch_character_description(character_name)
+
+puts "There are #{session.fetch_comic_number(character_name)} comics with #{character_name} in them."
+
+
+
   
--- marvel.rb
+++ marvel.rb
@@ -1,19 +1,22 @@
 require "marvelite"
 
 class Marvel
-  def initialize()
+  
+  def initialize(character_name)
     @client = Marvelite::API::Client.new( :public_key => '2b4b1e601cc382721ce8b4fceb18494c', 
     :private_key => 'b83361c174682e6fc1488982737c9c564002657a')
+    @hero = @client.character(character_name)
   end
   
   def check_character_name(character_name)
-    hero = @client.character(character_name)
-    hero[:code]
+    @hero[:code]
   end
   
   def fetch_character_description(character_name)
-    @hero = @client.character(character_name)
     @hero.data[:results][0][:description]
   end
-    
+  
+  def fetch_comic_number(character_name)
+    @hero.data[:results][0][:comics][:available]
+  end
 end
